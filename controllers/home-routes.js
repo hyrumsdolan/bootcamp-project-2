320c86adb9ba391c70a76b4dc20d1cc281122bf4
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Air.local>
2024-02-01 19:00:07 -0700

    add handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,11 @@
 // routes for homepage
 
 const router = require("express").Router();
+
+// get render login
+router.get("/", (req, res) => {
+  
+  res.render("login");
+});
+
+module.exports = router;
