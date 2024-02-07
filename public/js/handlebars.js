f16325b955309673b95ca669bf7f12b65892589f
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Air.local>
2024-02-06 19:18:19 -0700

    added IDs to various html elements

--- public/js/handlebars.js
+++ public/js/handlebars.js
@@ -1,37 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed"); // Debugging line
-    const form = document.getElementById("login-form");
-    if (form) {
-        form.addEventListener("submit", function(event) {
-            event.preventDefault();
-            
 
-            const formData = new FormData(form);
-            const data = {};
-            formData.forEach((value, key) => {
-                data[key] = value;
-            });
-            
-      
-            console.log(data); 
-        });
-    } else {
-        console.log("Form not found"); 
-    }
+
+
+
+
+
+
 });
-
-
-const submitButton = document.getElementById("submit");
-if (submitButton) {
-    submitButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        fetchExercisesForMuscle('biceps');
-        console.log("Submit button clicked"); 
-    });
-}
-
-
-  
-  // Example usage
-  fetchExercisesForMuscle('biceps');
-  
