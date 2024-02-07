97bf4a02b47fc5a085f6224a611625d39394b634
Kyler Jansson <kylerjansson@gmail.com>
2024-02-06 20:22:37 -0700

    added logout form handler

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,29 @@
 const loginFormHandler = async (event) => {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-  
-    // Get user input values from the login form
-    const email = document.querySelector('#login-email').value.trim();
-    const password = document.querySelector('#login-password').value.trim();
-  
-    if (email && password) {
-      // Send a POST request to the server with user login credentials
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        console.log("successful");
-        // document.location.replace('/');
-      } else {
-        console.log("Unsuccessfull");
-        alert('Failed to log in');
-      }
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get user input values from the login form
+  const email = document.querySelector('#login-email').value.trim();
+  const password = document.querySelector('#login-password').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the server with user login credentials
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      console.log("successful");
+      // document.location.replace('/');
+    } else {
+      console.log("Unsuccessful");
+      alert('Failed to log in');
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
