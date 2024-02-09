3945b4fc7bd3822180e76b2dd3c9ed16601345b6
Jesse Nay <jessenay26@gmail.com>
2024-02-09 11:13:57 -0700

    fixed register form handler and deleted the handlebars that were no longer needed

--- public/js/register.js
+++ public/js/register.js
@@ -1,31 +1,41 @@
 const registerFormHandler = async (event) => {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-  
-    // Get user input values from the login form
-    const name = document.querySelector('#create-account-username').value.trim();
-    const email = document.querySelector('#create-account-email').value.trim();
-    const password = document.querySelector('#create-account-password').value.trim();
-  
-    if (name && email && password) {
-      // Send a POST request to the server with user login credentials
-      const response = await fetch('/api/users/register', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        console.log("successfull")
-        document.location.replace('/');
-      } else {
-        console.log("Unsuccessfull");
-        alert('Failed to register');
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get user input values from the login form
+  const name = document.querySelector('#create-account-username').value.trim();
+  const email = document.querySelector('#create-account-email').value.trim();
+  const password = document.querySelector('#create-account-password').value.trim();
+
+  if (name && email && password) {
+      try {
+          // Send a POST request to the server with user login credentials
+          const response = await fetch('/api/users/register', {
+              method: 'POST',
+              body: JSON.stringify({ name, email, password }),
+              headers: { 'Content-Type': 'application/json' },
+          });
+
+          if (response.ok) {
+              console.log("successful");
+              document.location.replace('/selection');
+          } else {
+              // Handle non-OK responses by logging them and showing an alert
+              console.log("Unsuccessful");
+              const responseData = await response.json(); // Assuming server sends a JSON response
+              alert(`Failed to register: ${responseData.message}`);
+          }
+      } catch (error) {
+          // Catch and handle errors that occur during the fetch operation
+          console.error("Registration error:", error);
+          alert('An error occurred during registration.');
       }
-    }
-  };
-  
-  document
-    .querySelector('.register-form')
-    .addEventListener('submit', registerFormHandler);
-  
+  } else {
+      // If not all form fields are filled, alert the user
+      alert('Please fill in all fields.');
+  }
+};
+
+document
+  .querySelector('.create-account')
+  .addEventListener('submit', registerFormHandler);
